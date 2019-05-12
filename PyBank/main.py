faed6e7e6049a953f9179e42a00311731daf796d
Julio56 <julio5664@aol.com>
2019-05-12 10:51:29 -0400

    updating

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,24 @@
 
+import os
+import csv
+
+dates = []
+value = []
+
+# with open('RUTJC201904DATA3/hw/week3/Instructions/PyBank/Resources/budget_data.csv', 'r') as file_handler:
+#     lines = file_handler.read()
+#     print(lines)
+#     print(type(lines))
+
+
+
+csvpath = os.path.join('..' ,'RUTJC201904DATA3/hw/week3/Instructions/PyBank/Resources/budget_data.csv')
+ 
+with open(csvpath, newline='') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print(row)
+
+print(str(len([0])))
+
